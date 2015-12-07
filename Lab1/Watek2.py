b3f2155f7a6f5f944b51b13405a459b3a46052bf
patrykcejlowski <patrykcejlowski@gmail.com>
2015-12-07 19:39:29 +0100

    Watki

---
+++ Lab1/Watek2.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import time
+import datetime
+import threading
+
+def oblicz(x):
+    time.sleep(x)
+    return x * x
+
+# dodajemy stempelek czasowy do komunikatu
+
+def log(message):
+    now = datetime.datetime.now().strftime("%H:%M:%S")
+    print "%s %s" % (now, message)
+
+# kazde zadanie obliczweniowe uruchomione jest w osobnym watku
+
+class WatekOblicz(threading.Thread):
+    def __init__(self, value):
+        threading.Thread.__init__(self)
+        self.value = value
+    def run(self):
+        result = oblicz(self.value)
+        log("%s -> %s" % (self.value, result))
+
+def main():
+    log("uruchamiam watek glowny")
+    WatekOblicz(3).start()
+    WatekOblicz(2.5).start()
+    WatekOblicz(1).start()
+    log("koniec watku głownego")
+
+if __name__ == "__main__":
+    main()
